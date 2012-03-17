de3cbba3ca5f5a0c45921abde8fa3b0074c3897f
Danial Afzal <iotasquared@gmail.com>
2012-03-17 15:57:33 -0700

    Organise output by fight

--- parser.py
+++ parser.py
@@ -26,8 +26,6 @@ class Event:
     self.ability = Ability(data[3])
     self.effect = Effect(data[4])
     self.value = Value(data[5])
-    print data[5]
-    print self.value.value
     self.roll = Roll(data[6])
 
 class Entity:
@@ -106,24 +104,38 @@ def main():
     events.append(Event(line))
 
   players = set([event.source.name for event in events])
-  for player in players:
-    combat = False
-    dmg = 0
-    for event in events:
-      if 'EnterCombat' in event.effect.detail:
-        combat = event.date
-        dmg = 0
-      if 'ExitCombat' in event.effect.detail:
-        if combat and dmg:
-          print '%s did %d in %s' % (player, dmg, str(event.date-combat))
-        combat = False
-        
-      if not combat:
-        continue
+  fights = []
+  fight = []
+  for event in events:
+    if 'EnterCombat' in event.effect.detail:
+      fight = []
+    if 'ExitCombat' in event.effect.detail:
+      fight.append(event)
+      fights.append(fight)
+      continue
+    fight.append(event)
 
-      if event.source.name == player and 'Damage ' in event.effect.detail:
-        dmg += event.value.value
-        
+  for i, fight in enumerate(fights):
+    duration = fight[-1].date - fight[0].date
+    print '\nCombat Statistics for Fight # %d (duration: %s)' %(i+1, str(duration))
+    print str(fight[0].date)  +"   ->   " + str(fight[-1].date)
+    for player in players:
+      dmg = 0
+      heal = 0
+      for event in fight:
+        if event.source.name == player and 'Damage ' in event.effect.detail:
+          dmg += event.value.value
+        if event.source.name == player and 'Heal ' in event.effect.detail:
+          heal += event.value.value
+      secs = duration.seconds
+      if not secs:
+        secs = 1
+      if dmg:
+        print '%s did %d damage in %s (%d DPS)' % (player, dmg, str(duration), dmg/secs)
+      if heal:
+        print '%s did %d heals in %s (%d HPS)' % (player, heal, str(duration), heal/secs)
+    
+          
   #print "\n".join([event.to_string for event in events])
 if __name__ == "__main__":
   main()
